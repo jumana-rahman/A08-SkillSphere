c073640d0aad29395fb44ca31a55c0e99069b5e6
jumana-rahman <jumanarahman242@outlook.com>
2026-05-08 12:18:42 +0600

    project setup and necessary packages installed

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-purple-700 text-white font-bold'>
+            Footer
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-green-700 text-white'>
+            Navbar
+        </div>
+    );
+};
+
+export default Navbar;
