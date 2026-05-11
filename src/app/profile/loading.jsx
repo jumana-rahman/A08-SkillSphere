b169cd9d86417b9b3cffd18b2b214ba08829ad2c
jumana-rahman <jumanarahman242@outlook.com>
2026-05-11 20:42:16 +0600

    loading added in profile page

---
+++ src/app/profile/loading.jsx
@@ -0,0 +1,10 @@
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
+
+export default function Loading() {
+
+    return (
+        <div>
+            <LoadingSpinner/>
+        </div>
+    );
+}
